53e82b83d026910bf94013f8c1a8715f5f4f68f1
Zak Chapman <zakchapman@rocketmail.com>
2025-07-18 15:03:09 -0600

    adding more files

---
+++ index.js
@@ -0,0 +1,7 @@
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then(httpResponse => httpResponse.json())
+//     .then(data => {
+//       console.log(data);
+//     })
+let pokemonData = require("./ditto.json")
+console.log(pokemonData)
